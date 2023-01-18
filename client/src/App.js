48928f1b06efb6eedb0471935886f6944454e005
Parth Puranik <82353255+Parthos5@users.noreply.github.com>
2023-01-18 10:17:18 +0530

    setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main.js'
+import Nav from './components/Nav.js'
+
+function App() {
+  return (
+    <div>
+    <Nav />
+    <Main />
+</div>
+  );
+}
+
+export default App;
