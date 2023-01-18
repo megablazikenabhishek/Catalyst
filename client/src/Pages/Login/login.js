06d3ec47bb1ac05ede32d3275b2467927c84c0cb
jagjeet <sappaljagjeet@gmail.com>
2023-01-18 09:57:30 +0530

    initial commit
    start

---
+++ client/src/Pages/Login/login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./login.css"
+const Login = () => {
+    return (
+<>
+<div className="main_container">
+{/* <div className="login_box"> */}
+<div className="flex-container">
+  <div className="content-container">
+    <div className="form-container">
+      <form>
+        <h1>
+          Login
+        </h1>
+        <br/><br/>
+        <span class="subtitle">USERNAME:</span>
+        <br/>
+        <input type="text" name="username" />
+        <br/>
+        <span class="subtitle">PASSWORD:</span>
+        <br/>
+        <input type="password" name="password"/>
+        <br/><br/>
+        <input type="submit" value="SUBMIT" class="submit-btn"/>
+      </form>
+    </div>
+  </div>
+</div>
+    
+            </div> 
+ {/* </div> */}
+ </>
+    );
+}
+
+export default Login;
