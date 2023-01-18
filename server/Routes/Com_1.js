c4186fe40584524e1501c600655d8b791b8136be
Restyled.io <commits@restyled.io>
2023-01-18 17:08:11 +0000

    Restyled by prettier

--- server/Routes/Com_1.js
+++ server/Routes/Com_1.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  var io = req.app.get('socketio');
+  var io = req.app.get("socketio");
   io.emit("connected", "hello");
-})
+});
 
 module.exports = router;
