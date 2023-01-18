8223ad4f5e26f641c2b920c0997e8229cf67a65c
Parth Puranik <82353255+Parthos5@users.noreply.github.com>
2023-01-18 10:23:34 +0530

    Homepage setup

---
+++ client/src/Pages/Home/Main.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Main(){
+    return(
+        <h1>This is main content</h1>
+    )
+}
---
+++ client/src/Pages/Home/Nav.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Nav(){
+    return (
+        <nav>
+        <h1>this is navbar</h1>
+
+        </nav>
+    )
+}
