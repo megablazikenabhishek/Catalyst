f95e331b8fce03046e21c8674e5c36f9b52c686a
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-01-18 22:32:29 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("./db/connection");
 const http = require("http");
 require("dotenv").config();
-const Chat = require("./models/Chat");
 const cors = require("cors");
 const socket = require("socket.io");
 
@@ -14,13 +13,14 @@ const io = socket(server);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use(express.static("public"));
 
-app.get("/testing", async(req, res, next)=>{
-    const data = await Chat.find({})
-    console.log(data);
-    res.status(200).json({data});
-})
+app.set("socketio", io);
 
+//routers
+const Com1 = require("./Routes/Com_1");
+
+app.use("/Com1", Com1);
 // app.get("/put", async(req, res, next)=>{
 //     const data = require("./populate");
 //     await Chat.create(data);
@@ -30,10 +30,6 @@ app.get("/testing", async(req, res, next)=>{
 const port = 4200||process.env.PORT;
 const start = async()=>{
     try {
-        // await new Promise(resolve=>{
-        //     require("./db/connection");
-        //     resolve();
-        // });
         server.listen(port, ()=>console.log(`server is listening to port ${port}........`));
     } catch (error) {
         console.log(error);
