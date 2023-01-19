141eab29e7cf8b76bef85dbf62c978f60b3de311
Parth Puranik <thosparth@gmail.com>
2023-01-20 01:19:53 +0530

    update

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -43,8 +43,8 @@ function Navigation() {
                                 }
                                 id="basic-nav-dropdown"
                             >
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.1">Video Chat</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Saved Messages</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
 
                                 <NavDropdown.Item>
