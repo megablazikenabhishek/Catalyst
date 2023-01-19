ed72369bc84b11b727d66c66232c24a44b44a2ac
Hamza Sayyed <hamzawp404@gmail.com>
2023-01-19 10:41:08 +0530

    video chat uploaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,51 @@
-import "./App.css";
+import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import Chat from "./Pages/Home/Chat.js";
-import Community from "./Pages/Home/Community.js";
-import Nav from "./Pages/Home/Nav.js";
-import Navvertical from "./Pages/Home/Navvertical.js";
+import VideoPlayer from './components/VideoPlayer';
+import Sidebar from './components/Sidebar';
+import Notifications from './components/Notifications';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Nav />
-      <div class="dashboard">
-        <Navvertical />
-        <Community />
-        <Chat />
-      </div>
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Video Chat</Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Sidebar>
+        <Notifications />
+      </Sidebar>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './Context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import './styles.css';
+
+ReactDOM.render(
+  <ContextProvider>
     <App />
-  </React.StrictMode>
+  </ContextProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
