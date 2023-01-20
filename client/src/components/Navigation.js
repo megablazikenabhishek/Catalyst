97483583586a85d06c2d0952cce8451793e944f3
Restyled.io <commits@restyled.io>
2023-01-20 03:54:05 +0000

    Restyled by prettier

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import {useSelector} from "react-redux";
-import {LinkContainer} from "react-router-bootstrap";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
 
 import logo from "../assets/logo.png";
-import {useLogoutUserMutation} from "../services/appApi";
+import { useLogoutUserMutation } from "../services/appApi";
 
 function Navigation() {
   const user = useSelector((state) => state.user);
@@ -15,57 +15,67 @@ function Navigation() {
     // redirect to home page
     window.location.replace("/");
   }
-    return (
-        // <Navbar bg="dark" varient="dark">
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <LinkContainer to="/">
-                    <Navbar.Brand>
-                        <img src={logo} style={
-    { width: 50, height: 50 }} alt="logo"/>
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        {!user && (
-                            <LinkContainer to="/login">
-                                <Nav.Link >Login</Nav.Link>
-                            </LinkContainer>
-                        )}
-                        <LinkContainer to="/chat">
-                            <Nav.Link>Chat</Nav.Link>
-                        </LinkContainer>
-                        {user && (
-                            <NavDropdown
-                                title={
-                                    <>
-                                        <img src={user.picture} style={
-    {
-      width: 30, height: 30, marginRight: 10, objectFit: "cover",
-          borderRadius: "50%"
-    }} />
-                                        {user.name}
-                                    </>
-                                }
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href="#action/3.1">Video Chat</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Saved Messages</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+  return (
+    // <Navbar bg="dark" varient="dark">
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img src={logo} style={{ width: 50, height: 50 }} alt="logo" />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            )}
+            <LinkContainer to="/chat">
+              <Nav.Link>Chat</Nav.Link>
+            </LinkContainer>
+            {user && (
+              <NavDropdown
+                title={
+                  <>
+                    <img
+                      src={user.picture}
+                      style={{
+                        width: 30,
+                        height: 30,
+                        marginRight: 10,
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                    />
+                    {user.name}
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  Video Chat
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Saved Messages
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
 
-                                <NavDropdown.Item>
-                                    <Button variant="danger" onClick={handleLogout}>
-                                        Logout
-                                    </Button>
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+                <NavDropdown.Item>
+                  <Button variant="danger" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation;
