738f8e80168b3a4f47a1fee63cd3b1b859abf725
Abhishek Upadhyay <96608611+megablazikenabhishek@users.noreply.github.com>
2023-01-20 20:29:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use('/users', userRoutes)
 require('./connection')
 
 const server = require('http').createServer(app);
-const PORT = 5001;
+const PORT = process.env.PORT||5001;
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
