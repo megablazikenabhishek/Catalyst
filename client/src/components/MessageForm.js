92dfdfa88813b9dc371bc020b17befa1cd397634
Hamza Sayyed <hamzawp404@gmail.com>
2023-01-20 10:14:49 +0530

    trash

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -8,7 +8,7 @@ import starfill from "../assets/starfill.png";
 import staroutline from "../assets/staroutline.png";
 import upvote from "../assets/upvote.png";
 import { AppContext } from "../context/appContext";
-
+import "./MessageForm.css";
 function MessageForm() {
   const [message, setMessage] = useState("");
   const user = useSelector((state) => state.user);
@@ -44,121 +44,71 @@ function MessageForm() {
 
   const todayDate = getFormattedDate();
 
-  socket.off("room-messages").on("room-messages", (roomMessages) => {
-    setMessages(roomMessages);
-  });
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!message) return;
-    const today = new Date();
-    const minutes =
-      today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
-    const time = today.getHours() + ":" + minutes;
-    const roomId = currentRoom;
-    socket.emit("message-room", roomId, message, user, time, todayDate);
-    setMessage("");
-  }
-  return (
-    <>
-      <div className="messages-output">
-        {user && !privateMemberMsg?._id && (
-          <div className="alert alert-info" id="headingchat">
-            {currentRoom} room
-          </div>
-        )}
-        {user && privateMemberMsg?._id && (
-          <>
-            <div className="alert alert-info conversation-info">
-              <div>
-                Your conversation with {privateMemberMsg.name}{" "}
-                <img
-                  src={privateMemberMsg.picture}
-                  className="conversation-profile-pic"
-                />
-              </div>
-            </div>
-          </>
-        )}
-        {!user && <div className="alert alert-danger">Please login</div>}
+    socket.off("room-messages").on("room-messages", (roomMessages) => {
+        setMessages(roomMessages);
+    });
 
-        {user &&
-          messages.map(({ _id: date, messagesByDate }, idx) => (
-            <div key={idx}>
-              <p className="alert alert-info text-center message-date-indicator">
-                {date}
-              </p>
-              {messagesByDate?.map(
-                ({ content, time, from: sender }, msgIdx) => (
-                  <div
-                    className={
-                      sender?.email == user?.email
-                        ? "message"
-                        : "incoming-message"
-                    }
-                    key={msgIdx}
-                  >
-                    <div className="message-inner">
-                      <div
-                        id="userinfo"
-                        className="d-flex align-items-center userinfo"
-                      >
-                        <div id="prochat">
-                          <img
-                            src={sender.picture}
-                            style={{
-                              width: 35,
-                              height: 35,
-                              objectFit: "cover",
-                              borderRadius: "50%",
-                              marginRight: 10,
-                            }}
-                          />
-                          <p className="message-sender">
-                            {sender._id == user?._id ? "You" : sender.name}
-                          </p>
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!message) return;
+        const today = new Date();
+        const minutes = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+        const time = today.getHours() + ":" + minutes;
+        const roomId = currentRoom;
+        socket.emit("message-room", roomId, message, user, time, todayDate);
+        setMessage("");
+    }
+    return (
+        <>
+            <div className="messages-output">
+                {user && !privateMemberMsg?._id && <div className="alert alert-info">You are in the {currentRoom} room</div>}
+                {user && privateMemberMsg?._id && (
+                    <>
+                        <div className="alert alert-info conversation-info">
+                            <div>
+                                Your conversation with {privateMemberMsg.name} <img src={privateMemberMsg.picture} className="conversation-profile-pic" />
+                            </div>
                         </div>
-                        <div className="messagediv">
-                          <img className="vote" src={upvote} alt="" />
-                          <img id="star" src={staroutline}></img>
+                    </>
+                )}
+                {!user && <div className="alert alert-danger">Please login</div>}
+
+                {user &&
+                    messages.map(({ _id: date, messagesByDate }, idx) => (
+                        <div key={idx}>
+                            <p className="alert alert-info text-center message-date-indicator">{date}</p>
+                            {messagesByDate?.map(({ content, time, from: sender }, msgIdx) => (
+                                <div className={sender?.email == user?.email ? "message" : "incoming-message"} key={msgIdx}>
+                                    <div className="message-inner">
+                                        <div className="d-flex align-items-center userinfo">
+                                            <img src={sender.picture} style={{ width: 35, height: 35, objectFit: "cover", borderRadius: "50%", marginRight: 10 }} />
+                                            <p className="message-sender">{sender._id == user?._id ? "You" : sender.name}</p>
+                                        </div>
+                                        <p className="message-content">{content}</p>
+                                        <p className="message-timestamp-left">{time}</p>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                      </div>
-                      <p className="message-content">{content}</p>
-                      <p className="message-timestamp-left">{time}</p>
-                    </div>
-                  </div>
-                )
-              )}
+                    ))}
+                <div ref={messageEndRef} />
             </div>
-          ))}
-        <div ref={messageEndRef} />
-      </div>
-      <Form onSubmit={handleSubmit}>
-        <Row>
-          <Col md={11}>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                placeholder="Your message"
-                disabled={!user}
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
-          </Col>
-          <Col md={1}>
-            <Button
-              variant="primary"
-              type="submit"
-              style={{ width: "100%", backgroundColor: "orange" }}
-              disabled={!user}
-            >
-              <i className="fas fa-paper-plane"></i>
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-    </>
-  );
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col md={11}>
+                        <Form.Group>
+                            <Form.Control type="text" placeholder="Your message" disabled={!user} value={message} onChange={(e) => setMessage(e.target.value)}></Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col md={1}>
+                        <Button variant="primary" type="submit" style={{ width: "100%", backgroundColor: "orange" }} disabled={!user}>
+                            <i className="fas fa-paper-plane"></i>
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    );
 }
 
 export default MessageForm;
